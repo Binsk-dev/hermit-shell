c1faf29f325a0f56a18ca89147300af583e7ac3e
binsk-dev <juckmyeol0716@gmail.com>
2024-07-28 01:25:22 +0900

    feat: Add empty options

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,46 @@
-use clap::Parser;
+use clap::{CommandFactory, Parser, Subcommand};
+use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 #[command(about = "Interactive cluster shell built with Rust")]
 #[command(version = "0.1.0", author = "Binsk lee")]
-#[command(next_line_help = true)]
+#[command(disable_help_flag = true)]
 struct Cli {
     /// Command to execute
     command: Option<String>,
+
+    /// Print help information
+    #[arg(long = "help", exclusive = true)]
+    help: bool,
+
+    /// Path to the hosts file("host:[:port] [user])
+    #[arg(short = 'h', long = "hosts", value_name = "FILE")]
+    hosts: Option<PathBuf>,
+
+    /// Output directory
+    #[arg(short = 'o', long = "outdir", value_name = "DIR")]
+    outdir: Option<PathBuf>,
+
+    /// User to login
+    #[arg(short = 'u', long = "user", value_name = "USER")]
+    user: Option<String>,
+
+    /// Options to pass to the ssh
+    #[arg(short = 'O', long = "option", value_name = "OPTION")]
+    options: Option<Vec<String>>,
+
+    /// Timeout in seconds
+    #[arg(short = 't', long = "timeout", value_name = "SECONDS")]
+    timeout: Option<u64>,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    /// Help subcommand
+    Help {
+        /// The command to get help for
+        command: Option<String>,
+    },
 }
 
 enum Color {
@@ -36,6 +70,11 @@ fn display(output: &str, command: &str) {
 
 fn main() -> Result<(), std::io::Error> {
     let cli = Cli::parse();
+
+    if cli.help {
+        let _ = Cli::command().print_help();
+        return Ok(());
+    }
 
     match cli.command {
         Some(command) => {
